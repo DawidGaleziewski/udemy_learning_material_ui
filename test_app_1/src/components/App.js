01b36f893595028eb0700048e2eeb57d3f2d2728
Dawid Gałęziewski <Dawidgalsino@vp.pl>
2021-05-02 13:55:03 +0200

    learned more on menus

--- test_app_1/src/components/App.js
+++ test_app_1/src/components/App.js
@@ -2,12 +2,25 @@ import React from 'react'
 import Header from './ui/Header'
 import {ThemeProvider} from '@material-ui/styles'
 import theme from './ui/Theme';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Header />
-      test
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={() => <div>Home</div>}/>
+          <Route exact path="/services" component={() => <div>services</div>}/>
+          <Route exact path="/customesoftware" component={() => <div>customesoftware</div>}/>
+          <Route exact path="/mobileapps" component={() => <div>mobileapps</div>}/>
+          <Route exact path="/websites" component={() => <div>websites</div>}/>
+          <Route exact path="/revolution" component={() => <div>revolution</div>}/>
+          <Route exact path="/about" component={() => <div>about</div>}/>
+          <Route exact path="/contact" component={() => <div>contact</div>}/>
+          <Route exact path="/estimate" component={() => <div>estimate</div>}/>
+        </Switch>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
--- test_app_1/src/components/ui/Header.js
+++ test_app_1/src/components/ui/Header.js
@@ -7,6 +7,8 @@ import {makeStyles} from '@material-ui/styles';
 import Tabs from '@material-ui/core/Tabs'
 import Tab from '@material-ui/core/Tab'
 import logo from '../../assets/logo.svg';
+import Button from '@material-ui/core/Button'
+import {Link} from 'react-router-dom'
 
 function ElevationScroll(props) {
     const {children, window} = props
@@ -29,7 +31,7 @@ const useStyles = makeStyles(theme => ({
     marginBottom: "3em"
   },
   logo: {
-    height: "7em"
+    height: "8em"
   },
   tabContainer: {
     marginLeft: 'auto'
@@ -38,25 +40,65 @@ const useStyles = makeStyles(theme => ({
     ...theme.typography.tab,
     minWidth: 10,
     marginLeft: "25px"
+  },
+  button: {
+    ...theme.typography.estimate,
+    borderRadius: "50px",
+    marginLeft: "50px",
+    marginRight: "50px",
+    height: "45px",
+  },
+  logoContainer: {
+    padding: 0
   }
 }))
 
 export default function Header(props) {
   const classes = useStyles()
+  const [value, setValue] = React.useState(0);
+
+  React.useEffect(()=> {
+    if(window.location.pathname === "/" && value !== 0){
+      setValue(0)
+    } else if(window.location.pathname === "/services" && value !== 1){
+      setValue(1)
+    } else if(window.location.pathname === "/revolution" && value !== 2){
+      setValue(2)
+    } else if(window.location.pathname === "/about" && value !== 3){
+      setValue(3)
+    } else if(window.location.pathname === "/contact" && value !== 3){
+      setValue(3)
+    }
+  }, [value])
+
+  const handleChange = (event, value) => {
+    setValue(value)
+  }
+
     return (
       <React.Fragment>
         <ElevationScroll>
             <AppBar position='fixed' color="primary">
                 {/* Toolbar helps stack content vertically */}
                 <Toolbar disableGutters>
-                 <img className={classes.logo} alt="company logo" src={logo} />
-                <Tabs className={classes.tabContainer}>
-                  <Tab className={classes.tab} label="Home" />
-                  <Tab className={classes.tab} label="Services" />
-                  <Tab className={classes.tab} label="The Revolution" />
-                  <Tab className={classes.tab} label="About Us" />
-                  <Tab className={classes.tab} label="Contact Us" />
+                 <Button disableRipple onClick={()=> setValue(0)} className={classes.logoContainer} component={Link} to="/">
+                  <img className={classes.logo} alt="company logo" src={logo} />
+                 </Button>
+
+                <Tabs onChange={handleChange} value={value} className={classes.tabContainer}
+                  indicatorColor="primary"
+                >
+                  <Tab component={Link}
+                  to="/" className={classes.tab} label="Home" />
+                  <Tab className={classes.tab}
+                  component={Link} to="/services" label="Services" />
+                  <Tab component={Link} to="/revolution" className={classes.tab} label="The Revolution" />
+                  <Tab component={Link} to="/about" className={classes.tab} label="About Us" />
+                  <Tab component={Link} to="/contact" className={classes.tab} label="Contact Us" />
                 </Tabs>
+                <Button variant="contained" color="secondary" className={classes.button}>
+                  Free&nbsp;Estimate
+                </Button>
                 </Toolbar>
             </AppBar>
         </ElevationScroll>
